19d0d93c8ff324c4dc3e33724687e0ccc8808498
mazda1999 <32159582+mazda1999@users.noreply.github.com>
2019-12-28 09:48:17 +0800

    作業2

---
+++ UnityProject/Assets/chuken.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chuken : MonoBehaviour
+{
+    public Animator ani;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            ani.SetTrigger("吃飯觸發器");
+        }
+        /*
+        if (Input.GetKey(KeyCode.R))
+        {
+            ani.SetBool("跑步開關",true);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            ani.SetBool("跑步開關",false);
+        }
+        */
+
+        ani.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+    }
+
+
+}
